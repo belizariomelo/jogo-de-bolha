88d4b39c611f50c4acc66be14f0c90656a6cac4a
belizariomelo <124016228+belizariomelo@users.noreply.github.com>
2023-04-20 14:40:54 -0300

    Update script.js

--- BOLHA/script.js
+++ BOLHA/script.js
@@ -4,12 +4,13 @@ var bubbles = [];
 var count = 0;
 var startButton = document.getElementById("start");
 var restartButton = document.getElementById("restart");
+var pointThreshold = 5; // define o limite de pontos para aumentar a velocidade
+var speedIncrease = 0.2; // define o aumento percentual da velocidade
 
 startButton.addEventListener("click", function () {
   startButton.style.display = "none";
   restartButton.style.display = "block";
-
-  setInterval(function () {
+  var bubbleInterval = setInterval(function () {
     var bubble = document.createElement("div");
     bubble.className = "bubble";
     var size = Math.floor(Math.random() * 100) + 50;
@@ -19,9 +20,46 @@ startButton.addEventListener("click", function () {
     bubble.style.left = Math.floor(Math.random() * 580) + "px";
     board.appendChild(bubble);
     bubbles.push(bubble);
-    if (bubbles.length > 10) {
+    if (bubbles.length > 11) {
       board.removeChild(bubbles[0]);
       bubbles.shift();
+      if (bubbles.length > 10) {
+        alert("PÔ VANESSA MAIS RÁPIDO"); // mostra alerta se houver mais de 10 bolhas no board
+        clearInterval(bubbleInterval);
+       
+      }
+    }
+    if (count >= pointThreshold) { // verifica se atingiu o limite de pontos
+      clearInterval(bubbleInterval); // interrompe o intervalo atual
+      var newSpeed = 1000 * (1 - speedIncrease); // calcula a nova velocidade
+      bubbleInterval = setInterval(function () {
+        // continua com o intervalo atual usando a nova velocidade
+        var bubble = document.createElement("div");
+        bubble.className = "bubble";
+        var size = Math.floor(Math.random() * 100) + 50;
+        bubble.style.width = size + "px";
+        bubble.style.height = size + "px";
+        bubble.style.top = Math.floor(Math.random() * 480) + "px";
+        bubble.style.left = Math.floor(Math.random() * 580) + "px";
+        board.appendChild(bubble);
+        bubbles.push(bubble);
+        if (bubbles.length > 11) {
+          board.removeChild(bubbles[0]);
+          bubbles.shift();
+          if (bubbles.length > 10) {
+            alert("PÔ VANESSA MAIS RÁPIDO"); // mostra alerta se houver mais de 10 bolhas no board
+            clearInterval(bubbleInterval);
+            restartButton.style.display = "none";
+            startButton.style.display = "block";
+            if (confirm("Game over!")) { // exibe uma caixa de confirmação ao invés de um alerta
+              location.reload(); // recarrega a página se o usuário clicar em "OK"
+             
+            }
+            
+          }
+        }
+      }, newSpeed);
+      pointThreshold += 10; // atualiza o limite de pontos
     }
   }, 1000);
 
